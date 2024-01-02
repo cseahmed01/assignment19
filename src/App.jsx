d5944172093cc910eaaf6b5e095c5cd3c4e34be1
Nasim Ahmed <35323045+cseahmed01@users.noreply.github.com>
2024-01-02 16:10:40 +0600

    finall adding

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import AppNav from './component/AppNav'
 import AppFooter from './component/AppFooter'
 import HomePage from './pages/HomePage'
+import ThankyouPage from './pages/ThankyouPage'
+import BlogPage from './pages/BlogPage'
+import CartPage from './pages/CartPage' 
+import CheckoutPage from './pages/CheckoutPage';
+import ContactPage from './pages/ContactsPage';
+import FaqPage from './pages/FaqPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Privacy from './pages/Privacy'
+import ProductsPage from './pages/ProductsPage';
+import RegistrationPage from './pages/RegistrationPage';
+import ReviewPage from './pages/ReviewPage';
+import SearchPage from './pages/SearchPage';
 
- 
 export default function App() {
   return (
     <>
@@ -12,6 +23,20 @@ export default function App() {
         <AppNav/>
         <Routes>
           <Route path="/" element={<HomePage/>} />
+          <Route path="/blog" element={<BlogPage/>}/>
+          <Route path="/cart" element={<CartPage/>}/>
+          <Route path="/checkout" element={<CheckoutPage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="/thankyou" element={<ThankyouPage/>}/>
+          <Route path="/faq" element={<FaqPage/>}/>
+          <Route path="/Privacy" element={<Privacy/>}/>
+          <Route path="/products" element={<ProductsPage/>} />
+          <Route path="/registration" element={<RegistrationPage/>} />
+          <Route path="/Review" element={<ReviewPage/>} />
+          <Route path="/search" element={<SearchPage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        
+ 
         </Routes>
         <AppFooter/>
       </BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
- 
+import 'jquery';
+import '../../assignment19/src/assets/css/plugins.min.css'
+import '../../assignment19/src/assets/css/style.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
